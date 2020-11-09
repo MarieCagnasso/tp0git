c417f56a8af6b1c4697be0a22b50dbc0836737f4
MarieCagnasso <marie.cagnasso@etud.univ-jfc.fr>
2020-11-09 15:25:16 +0100

    c'est la fête.

--- src/main/java/maclass.java
+++ src/main/java/maclass.java
@@ -2,5 +2,6 @@ public class maclass {
     public maclass(){
             //Salut on s'amuse oulala
             //cest super cest comme l'an dernier :D
+            //Et l'année encore d'avant
     }
 }
